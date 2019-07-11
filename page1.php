4f98d6ca2566e0ae2d4994dd484be52ad67ca5ea
Neelu <neelu.rsa@gmail.com>
2019-07-11 09:12:21 +0100

    stuff

--- page1.php
+++ page1.php
@@ -9,15 +9,20 @@
   <title>MyRanker</title>
 </head>
 <body>
-  <h1>Input your A-Levels</h1>
-  <form>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  <div class="heading">
+    <h1>Input your A-Levels and predicted grades</h1>
+  </div>
+  <div class="container main">
+    <div class="alevel-container container"></div>
+    <div class="text-center mb-2">
+      <button class="btn btn-danger text-center" onclick="createALevel();">Add subject</button>
     </div>
+    <div class="text-center mb-2 show-prefs hidden">
+      <button class="btn btn-danger text-center" onclick="showPrefs();">Next &rarr;</button>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+
+  </div>
+
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,46 @@
+var listOfALevels = [
+  'Mathematics',
+  'Further Mathematics',
+  'Computer Science',
+  'Physics'
+];
+
+var listOfGrades = [
+  'A*',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E'
+];
+
+var counter = 0;
+
 function createALevel() {
-  var 
+  var alevel = $('<div class="alevel input-group row c-' + counter + '"></div>');
+  var subjectSelect = $('<select class="custom-select col-8 subject-select"></select>');
+  subjectSelect.append($('<option selected>Subject</option>'));
+  for (i = 0; i < listOfALevels.length; i++) {
+    subjectSelect.append($('<option value="' + listOfALevels[i] + '">' + listOfALevels[i] + '</option>'));
+  }
+  var gradeSelect = $('<select class="custom-select col-2 grade-select"></select>');
+  gradeSelect.append($('<option selected>Predicted grade</option>'));
+  for (i = 0; i < listOfGrades.length; i++) {
+    gradeSelect.append($('<option value="' + listOfGrades[i] + '">' + listOfGrades[i] + '</option>'));
+  }
+  var spacer
+  var removeSubject = $('<button class="btn btn-danger col-1 offset-1 remove-subject" onclick="removeALevel(' + counter + ')">Remove</button>');
+  alevel.append(subjectSelect);
+  alevel.append(gradeSelect);
+  alevel.append(removeSubject);
+  $('.alevel-container').append(alevel);
+  $('.show-prefs').removeClass('hidden');
+  counter += 1;
+}
+
+function removeALevel(c) {
+  $('.c-' + c).remove();
+  if ($('.alevel').length == 0) {
+    $('.show-prefs').addClass('hidden');
+  }
 }
